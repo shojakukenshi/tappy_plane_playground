8341d8d29c47fb72ced304efef3dd43b893f5b75
Kenshi Shojaku <shojaku@gmail.com>
2018-06-06 09:30:23 +0900

    use gravity for plane movement

--- TappyPlanePlaygroundGame.playground/Contents.swift
+++ TappyPlanePlaygroundGame.playground/Contents.swift
@@ -10,7 +10,6 @@ class GameScene: SKScene {
     private var plane : SKShapeNode!
     private var gameOverLabel : SKLabelNode!
     
-    private var yVelocity = 10.0 as CGFloat
     private var distance = 0
     private var inGame = true
     
@@ -30,7 +29,8 @@ class GameScene: SKScene {
         plane.fillColor = SKColor.white
         plane.position = CGPoint(x: self.frame.midX - self.frame.size.width / 6.0, y: self.frame.midY)
         plane.physicsBody = SKPhysicsBody(circleOfRadius: max(w / 2, w / 2))
-        plane.physicsBody?.affectedByGravity = false
+        plane.physicsBody?.velocity = CGVector(dx: 0.0, dy: 200.0)
+        plane.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 200.0))
     
         addChild(plane)
         
@@ -46,7 +46,8 @@ class GameScene: SKScene {
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        yVelocity = 10.0
+        plane.physicsBody?.velocity = CGVector(dx: 0.0, dy: 0.0)
+        plane.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 100.0))
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -62,8 +63,6 @@ class GameScene: SKScene {
         if !inGame {
             return
         }
-        yVelocity -= 0.3
-        plane.position = CGPoint(x: plane.position.x, y: plane.position.y + yVelocity)
         if plane.position.x < self.frame.midX - self.frame.size.width / 3.2 || plane.position.y < self.frame.midY - self.frame.size.height / 2.0 {
             inGame = false
             gameOverLabel = SKLabelNode(text: "SCORE \(distance)")
@@ -71,7 +70,7 @@ class GameScene: SKScene {
             addChild(gameOverLabel)
         }
         
-        if distance % 50 == 0 {
+        if distance % 70 == 0 {
             let wall_clone = wall.copy() as! SKShapeNode
             var wallYPosition = self.frame.midY - self.frame.size.height * (0.25 + CGFloat(arc4random_uniform(51)) / 100.0)
             if arc4random_uniform(2) >= 1 {
@@ -89,6 +88,8 @@ let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 480, height: 640))
 if let scene = GameScene(fileNamed: "GameScene") {
     // Set the scale mode to scale to fit the window
     scene.scaleMode = .aspectFill
+    
+    scene.physicsWorld.gravity = CGVector(dx: 0.0, dy: -2.0)
     
     // Present the scene
     sceneView.presentScene(scene)
